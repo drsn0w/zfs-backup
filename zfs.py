6f13cbc6ce222a33372e27cd6b11f176dcbe4dba
Liam Crabbe <liam.crabbe.3@gmail.com>
2020-06-13 21:33:35 -0500

    zfs.py: rebuild simple zfs library around libzfs_core Python3 bindings

--- zfs.py
+++ zfs.py
@@ -1,51 +1,26 @@
 import logging,coloredlogs
+import libzfs_core as libzfs
+import libzfs_core.exceptions as lzce
 import subprocess
 import sys
 coloredlogs.install(level='DEBUG')
 
 def checkExists(datasetName):
-    logging.debug("checkExists checking for dataset " + datasetName)
-    datasetsByte = subprocess.check_output(['zfs', 'list','-H'])
-    datasets = datasetsByte.decode("utf-8")
-    if datasetName in datasets:
-        logging.debug("checkExists found " + datasetName)
-        return True
-    else:
-        logging.debug("checkExists did not find " + datasetName)
-        return False
-
-def checkExistsSnapshot(datasetName):
-    logging.debug("checkExists checking for dataset " + datasetName)
-    datasetsByte = subprocess.check_output(['zfs', 'list', '-t snapshot''-H'])
-    datasets = datasetsByte.decode("utf-8")
-    if datasetName in datasets:
-        logging.debug("checkExists found " + datasetName)
-        return True
-    else:
-        logging.debug("checkExists did not find " + datasetName)
-        return False
+    datasetNameB = str.encode(datasetName)
+    return(libzfs.lzc_exists(datasetNameB))
 
 def create(datasetName):
-    if checkExists(datasetName):
+    datasetNameB = str.encode(datasetName)
+    try:
+        libzfs.lzc_create(datasetNameB)
+    except lzce.FilesystemExists:
         logging.fatal(datasetName + " already exists! Exiting.")
-        sys.exit(1)
-    else:
-        logging.debug("creating dataset " + datasetName)
-        zfsReturnCode = subprocess.call(['zfs', 'create', datasetName])
-        if zfsReturnCode != 0:
-            logging.fatal("zfs create failed!")
-            sys.exit(1)
-            
+        exit(1)
+    except (lzce.NameInvalid, lzce.NameTooLong):
+        logging.fatal(datasetName + " is an invalid dataset name!")
+
+def snapshot(datasetName, snapshotName):
+    logging.debug("making snapshot " + snapshotName + " in " + datasetName)
+
 def sendLocal(sourceSnapshot, destDataset):
-    if not checkExistsSnapshot(sourceSnapshot):   
-        logging.fatal(sourceSnapshot + " does not exist! Exiting.")
-        sys.exit(1)
-
-    if not checkExists(destDataset):
-        logging.fatal(destDataset + "does not exist! Exiting.")
-        sys.exit(1)
-
-def sendRemote(sourceSnapshot, destHost, destDataset):
-        if not checkExistsSnapshot(sourceSnapshot):   
-            logging.fatal(sourceSnapshot + " does not exist! Exiting.")
-            sys.exit(1)
+    logging.debug("sending " + sourceSnapshot + " to " + destDataset)
